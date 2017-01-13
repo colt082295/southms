bc8e649ac2b282b58e238c7906da6dcfeb96b21c
Colt <colt082295@gmail.com>
2017-01-13 15:52:46 -0600

    API changes.

--- craft/config/elementapi.php
+++ craft/config/elementapi.php
@@ -3,7 +3,7 @@ namespace Craft;
 
 return [
     'endpoints' => [
-        'api/filter' => [
+        'api/filter.json' => [
           'elementType' => 'Entry',
             'criteria' => ['section' => 'filter'],
             'transformer' => function(EntryModel $entry) {
@@ -14,6 +14,62 @@ return [
                     'jsonUrl' => UrlHelper::getUrl("filter/{$entry->slug}.json"),
                 ];
             },
-        ]
+        ],
+        'api/jobs.json' => [
+            'elementType' => 'Entry',
+            'criteria' => ['section' => 'jobs'],
+            'transformer' => function(EntryModel $entry) {
+                return [
+                    'title' => $entry->title,
+                    'position' => $entry->position,
+                    'url' => $entry->url,
+                    'jsonUrl' => UrlHelper::getUrl("jobs/{$entry->id}.json")
+                ];
+            },
+        ],
+        'api/jobs/<entryId:\d+>.json' => function($entryId) {
+            return [
+                'elementType' => 'Entry',
+                'criteria' => ['id' => $entryId],
+                'elementsPerPage' => 15,
+                'first' => true,
+                'transformer' => function(EntryModel $entry) {
+                    return [
+                        'title' => $entry->title,
+                        'url' => $entry->url,
+                        'body' => $entry->body,
+                        'position' => $entry->position,
+                    ];
+                },
+            ];
+        },
+        'api/events.json' => [
+            'elementType' => 'Entry',
+            'criteria' => ['section' => 'events'],
+            'transformer' => function(EntryModel $entry) {
+                return [
+                    'title' => $entry->title,
+                    'eventName' => $entry->eventName,
+                    'url' => $entry->url,
+                    'jsonUrl' => UrlHelper::getUrl("events/{$entry->id}.json")
+                ];
+            },
+        ],
+        'api/events/<entryId:\d+>.json' => function($entryId) {
+            return [
+                'elementType' => 'Entry',
+                'criteria' => ['id' => $entryId],
+                'elementsPerPage' => 15,
+                'first' => true,
+                'transformer' => function(EntryModel $entry) {
+                    return [
+                        'title' => $entry->title,
+                        'eventName' => $entry->eventName,
+                        'url' => $entry->url,
+                        'body' => $entry->body,
+                    ];
+                },
+            ];
+        },
     ]
 ];
