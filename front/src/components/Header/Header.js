c27f555243363d3398c097aa8331c47b793cfbc5
Colt <colt082295@gmail.com>
2017-01-12 23:18:30 -0600

    add more

---
+++ front/src/components/Header/Header.js
@@ -0,0 +1,65 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Header.css';
+import Link from '../Link';
+import Navigation from '../Navigation';
+import logoUrl from './logo-small.png';
+import logoUrl2x from './logo-small@2x.png';
+import { Menu } from 'semantic-ui-react';
+
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { /* initial state */ };
+  }
+
+  handleItemClick() {
+
+
+    
+  }
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu>
+        <Menu.Item
+          name='editorials'
+          active={activeItem === 'editorials'}
+          onClick={this.handleItemClick}
+        >
+          Editorials
+        </Menu.Item>
+
+        <Menu.Item
+          name='reviews'
+          active={activeItem === 'reviews'}
+          onClick={this.handleItemClick}
+        >
+          Reviews
+        </Menu.Item>
+
+        <Menu.Item
+          name='upcomingEvents'
+          active={activeItem === 'upcomingEvents'}
+          onClick={this.handleItemClick}
+        >
+          Upcoming Events
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
+
+export default withStyles(s)(Header);
