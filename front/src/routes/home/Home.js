99a4f1fb7021f863c42989be63df265b8d5777c5
Colt <colt082295@gmail.com>
2017-01-31 09:22:20 -0600

    More updates.

--- front/src/routes/home/Home.js
+++ front/src/routes/home/Home.js
@@ -1,36 +1,18 @@
 import React, { PropTypes } from 'react';
+import Home from '../../components/Home/Render';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 
-class Home extends React.Component {
-  static propTypes = {
-    news: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      link: PropTypes.string.isRequired,
-      contentSnippet: PropTypes.string,
-    })).isRequired,
-  };
-
+class HomeWrapper extends React.Component {
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <h1>Homepage</h1>
-          <ul className={s.news}>
-            {this.props.news.map((item, index) => (
-              <li key={index} className={s.newsItem}>
-                <a href={item.link} className={s.newsTitle}>{item.title}</a>
-                <span
-                  className={s.newsDesc}
-                  dangerouslySetInnerHTML={{ __html: item.contentSnippet }}
-                />
-              </li>
-            ))}
-          </ul>
+          <Home />
         </div>
       </div>
     );
   }
 }
 
-export default withStyles(s)(Home);
+export default withStyles(s)(HomeWrapper);
